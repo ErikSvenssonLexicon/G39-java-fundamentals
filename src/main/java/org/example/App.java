afb2b46af3d9747c2ad0a218ed18ae3a35171892
Erik Svensson <eosvensson@gmail.com>
2021-10-05 11:46:24 +0200

    Fundamental lecture part 1

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+public class App 
+{
+    public static void main( String[] args ) { // Start of main method
+        int modulus = 101 % 2;
+        System.out.println(modulus);
+
+    } // End of main method
+}
